dfb83abfc77fe5f5222759bcdafbd5bb72c5b423
rammses <mesutbayrak@gmail.com>
2021-01-03 21:37:41 +0300

    changes in user panel

--- app.py
+++ app.py
@@ -1,63 +1,8 @@
 from flask_table import Table, Col, LinkCol, ButtonCol
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from tools.database import UserData
-import pymysql
-
-"""A example for creating a simple table within a working Flask app.
-Our table has just two columns, one of which shows the name and is a
-link to the item's page. The other shows the description.
-"""
 
 app = Flask(__name__)
-
-
-class ItemTable(Table):
-    # name = LinkCol('Owner', 'single_item',
-    #                url_kwargs=dict(id='id'), attr='name')
-    name = Col('Owner')
-    description = Col('Labname')
-
-    power = ButtonCol(name='Power',
-                    endpoint='poweroff',
-                    url_kwargs=dict(uuid='id'),
-                    button_attrs=dict(name='Power-On',)
-                    )
-
-class Item(object):
-    """ a little fake database """
-    def __init__(self, id, name, description):
-        self.id = id
-        self.name = name
-        self.description = description
-
-    @classmethod
-    def get_elements(cls):
-        return [
-            Item(1, 'mesut@mikronet.net', 'base-2243*0984203*942209348'),
-            Item(2, 'hakan@mikronet.net', 'base-nmöbb234mn23bmn234b234'),
-            Item(3, 'mesut@mikronet.net', 'base-2243*09842234m234nmsnb8')]
-
-    @classmethod
-    def get_element_by_id(cls, id):
-        return [i for i in cls.get_elements() if i.id == id][0]
-
-
-@app.route('/')
-def index():
-    items = Item.get_elements()
-    table = ItemTable(items)
-
-    # You would usually want to pass this out to a template with
-    # render_template.
-    return table.__html__()
-
-
-@app.route('/item/<int:id>')
-def single_item(id):
-    element = Item.get_element_by_id(id)
-    # Similarly, normally you would use render_template
-    return '<h1>{}</h1><p>{}</p><hr><small>id: {}</small>'.format(
-        element.name, element.description, element.id)
 
 @app.route('/PowerOff/<uuid>')
 def poweroff(uuid):
@@ -68,7 +13,7 @@ def poweron(uuid):
     return uuid
 
 @app.route('/controlpanel')
-def test():
+def admin_panel():
     database = UserData(server='192.168.17.131',
                     user='root',
                     password='12qwasZX',
@@ -77,7 +22,28 @@ def test():
     all_labs = database.get_all_labs()
     data = database.get_formatted_lab_data(all_labs)
 
-    return render_template('table.html', headings=headings, data=data)
+    return render_template('admin_panel.html', headings=headings, data=data)
+
+
+@app.route('/users/', methods=["POST", "GET"])
+def user_land():
+    if request.method == 'GET':
+        return render_template('user_landing.html')
+    elif request.method == 'POST':
+        user_mail=request.form['email']
+        return redirect(url_for('user_panel', user_mail=user_mail))
+
+@app.route('/users/<user_mail>')
+def user_panel(user_mail):
+    database = UserData(server='192.168.17.131',
+                        user='root',
+                        password='12qwasZX',
+                        database='core_labs')
+    headings = ('new_lab_id', 'started on', 'expire on', 'ports', 'running on', 'status')
+    users_labs = database.get_users_labs(user_mail)
+    data = database.get_formatted_user_lab_data(users_labs)
+    return render_template('user_panel.html', headings=headings, data=data)
+
 
 
 if __name__ == '__main__':
--- tools/database.py
+++ tools/database.py
@@ -27,6 +27,13 @@ class UserData:
         else:
             return False
 
+    def get_lab_name(self, gns3_server_ip, lab_uuid):
+        uri = 'http://' + str(gns3_server_ip) + ':3080/v2/projects/' + lab_uuid
+        # print(uri)
+        response = requests.get(uri)
+        details = json.loads(response.content)
+        return details['name']
+
     def get_mail(self, fk_user_id):
         # Connect to the database
         try:
@@ -40,6 +47,21 @@ class UserData:
             print('error on get_mail :', e)
 
         return result['email']
+
+    def get_user_id(self, users_mail):
+        # Connect to the database
+        sql_fixed_user_mail = '\''+users_mail+'\''
+        try:
+            with self.connection.cursor() as cursor:
+                # Read a single record
+                sql = "SELECT pk_user_id FROM user WHERE email=" + sql_fixed_user_mail
+                print('doing :', sql)
+                cursor.execute(sql)
+                result = cursor.fetchone()
+        except Exception as e:
+            print('error on get_user_id :', e)
+
+        return result['pk_user_id']
 
     def get_all_labs(self):
 
@@ -77,6 +99,54 @@ class UserData:
                     gns3_ip = v
                 elif k == 'lab_name':
                     lab_name = v
+                elif k == 'new_lab_id':
+                    new_lab_id = v
+                if gns3_ip is not None and new_lab_id is not None:
+                    if self.get_lab_status(gns3_server_ip=gns3_ip, lab_uuid=new_lab_id) is True:
+                        render_member.append('running')
+                    else:
+                        render_member.append('stopped')
+            render_data.append(tuple(render_member))
+        return tuple(render_data)
+
+    def get_users_labs(self,user_mail):
+        fk_user_id = self.get_user_id(user_mail)
+        # Connect to the database
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "SELECT new_lab_id, lab_created_at, lab_expires_at, lab_ports_start, running_on FROM core_labs.lab" \
+                      " WHERE fk_user_id =" + str(fk_user_id)
+                print('doing :', sql)
+                cursor.execute(sql)
+                users_labs = cursor.fetchall()
+        except Exception as e:
+            print('error on get all labs :',e)
+        return users_labs
+
+
+    def get_formatted_user_lab_data(self, users_labs):
+        render_data = []
+        for lab in users_labs:
+            render_member = []
+            gns3_ip = None
+            lab_name = None
+            new_lab_id = None
+            for k, v in lab.items():
+                if k == 'fk_user_id':
+                    users_mail = self.get_mail(fk_user_id=str(v))
+                    render_member.append(users_mail)
+                elif isinstance(v, datetime.datetime):
+                    string_it = v.strftime("%m/%d/%Y, %H:%M:%S")
+                    render_member.append((string_it))
+                else:
+                    render_member.append(v)
+            # statü sor
+            for k, v in lab.items():
+                if k == 'running_on':
+                    gns3_ip = v
+                elif k == 'lab_name':
+                    lab_name = v
+                    print('lab_name is : ', lab_name )
                 elif k == 'new_lab_id':
                     new_lab_id = v
                 if gns3_ip is not None and new_lab_id is not None:
